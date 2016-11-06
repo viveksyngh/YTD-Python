e10d4cc4dc0afc8c339f50744197c89d704e7ca9
Vivek Singh <vivek@happay.in>
2016-11-06 21:16:07 +0530

    initial commit

---
+++ downloader.py
@@ -0,0 +1,36 @@
+import os
+import time
+from pytube import YouTube
+import os
+
+def download_video(video_url):
+    y = YouTube()
+    y.url = video_url
+
+    #y.filename contains the default file name
+    if not os.path.isfile(DOWNLOAD_PATH+y.filename+'.mp4'):
+        print "Downloading %s: " % y.filename
+        video = y.get("mp4", "720p")
+        video.download(DOWNLOAD_PATH)
+        return
+    print "Aborting: Already downloaded %s" % y.filename
+
+
+def get_video_list(filepath):
+    return open(filepath, "r").read().splitlines()
+
+if __name__ == "__main__":
+    filepath = 'video_urls.txt'
+    
+    ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+    DOWNLOAD_PATH = ROOT_PATH + "/videos/"
+    if not os.path.exists(DOWNLOAD_PATH):
+        os.mkdir(DOWNLOAD_PATH)
+    
+    start_time = time.time()
+    for video_url in get_video_list(filepath):
+        download_video(video_url)
+
+    time_taken = time.time()-start_time
+    print "Time Taken: %s" % time_taken
+    print "Finished downloading all pycon videos!"
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Youtube Video Downloader Python
+
+## Clone this Repository
+## Update the urls of Youtube videos in video_urls.txt 
+## Run the script with python downloader.py from root directory
